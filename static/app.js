6704193f66b0b15f35adde64ad024bf495124197
Andy Evans <andrewevans0@outlook.com>
2017-10-29 13:08:47 +0000

    typo

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 // Functions
 
-function moveFowards() {
+function moveForwards() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
