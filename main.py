82930211de3786b6a3ddfeaf0abec7b085e2b8b3
Andy Evans <andrewevans0@outlook.com>
2017-10-29 12:42:02 +0000

    added backwards

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import time
 from time import sleep
 from flask_cors import CORS, cross_origin
 import forwards
+import backwards
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/forwards', methods=['GET'])
 def move_forwards():
     return forwards.move()
+
+@app.route('/backwards', methods=['GET'])
+def move_forwards():
+    return backwards.move()
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
