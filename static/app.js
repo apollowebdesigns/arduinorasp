cb40547a35138edbbcbfb7869e7f53befb7dea2d
Andy Evans <andrewevans0@outlook.com>
2017-10-29 13:05:57 +0000

    added javascript

---
+++ static/app.js
@@ -0,0 +1,12 @@
+// Functions
+
+function moveFowards() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("fowardsResponse").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/forwards", true);
+  xhttp.send();
+}
